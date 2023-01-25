fadd86c48cdd4251bfb98fefc11919b510d3d553
Grant Solomon <grantsolomon.08.25@gmail.com>
2023-01-25 16:33:42 -0500

    fixing initial state functions

--- main.py
+++ main.py
@@ -5,32 +5,38 @@ board = [[1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1], 
         [1, 1, 1, 1, 1, 1, 1], 
         [1, 1, 1, 1, 1, 1, 1], 
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1]] #7x7 Board
+        [1, 0, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],] #7x7 Board
 
 
 #SET-UP METHODS
 def dead_state(width, height):
-    dead_array = [] * height
-    for i in range(height):
-        dead_array[i] = [0] * width
-        return dead_array
+    dead_array = [0] * height
 
-def random_state(width, height, probability_param: float =0.5):
-    state: list[list[int]] = dead_state(width, height)
+    for i in range(len(dead_array)):
+        dead_array[i] = [0] * width
+
+    return dead_array 
+
+
+def random_state(width, height, probability_param=0.5):
+    state = dead_state(width, height)
     for i in range(len(state)):
-        random_num = random.Random(0, 1)
+        random_num = Random(0, 1)
         if random_num >= probability_param:
             state[i][i] = 0
         else: 
             state[i][i] = 1
-        return state
+    return state
     
-def print_state(board, width=False, height=False):
+def print_state(board, width=0, height=0):
     if width > 0 and height > 0:
         print("This is a board of width and height")
-    print(board)
+        print(board)
+    else: 
+        ValueError("""This Board does not have a width and height greater
+        than zero making it unprintable""")
+
     
 def pretty_print_state():
     pass
